33ec9f9ba923614a4cced575a81f4bdb5ef3abba
humaira-afrin <147653772+humaira-afrin@users.noreply.github.com>
2024-01-24 23:13:49 +0100

    Add files via upload

--- hexmain.asm
+++ hexmain.asm
@@ -3,37 +3,38 @@
   # Copyright abandonded - this file is in the public domain.
 
 	.text
-	.globl hexasc
 
 	
 main:
-	li	$a0,5		# change this to test different values
+	li	$a0,13		# change this to test different values
 	jal	hexasc		# call hexasc
 	nop			# delay slot filler (just in case)	
 
 	move	$a0,$v0		# copy return value to argument register
 	li	$v0,11		# syscall with v0 = 11 will print out
-	syscall			# one byte from a0 to the Run I/O window
+	syscall	
+			# one byte from a0 to the Run I/O window
 	
 stop:	j	stop		# stop after one run
 	nop			# delay slot filler (just in case)
 
   # You can write your own code for hexasc here
   hexasc:
-  	li $t0, 0xF
-  	add $a0, $a0, $t0 #mask 4 LSb from a0
+  	li $t0, 15
+  	and $t1, $a0, $t0 #mask 4 LSb from a0
   #is last 4 0-9
- 	li $t1, 0x9
- 	#slt $t3, $a0, $t2
- 	#bne $t3, 1, is_letter #if 3 is not less than 9--> letter
- 	blt $a0, $t1, is_letter
- 	addi $v0, $a0, 48
+ 	li $t2, 9
+ 	slt $t3, $t1, $t2
+ 	bne $t3, 1, is_letter #if 3 is not less than 9--> letter
+ 	addi $t1, $t1, 48
  	j end_hexa
  
  
  is_letter:
- 	addi $v0, $a0, 55
+ 	addi $t1, $t1, 55
  
- end_hexa: jr $ra
+ end_hexa: 
+ 	move $v0, $t1
+ 	jr $ra
  	nop
  
